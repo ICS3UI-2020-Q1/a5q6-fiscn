2dcc58a44c283e6fbe18821903b52324fe889644
fiscn <fiscn2709@wrdsb.ca>
2020-10-06 16:23:01 +0000

    Finished A5 Q6 program

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 /**
- *
- * @author 
+ * prints a box of stars based on the number provided by the user
+ * @author Neil Fischer
  */
 public class Main {
 
@@ -9,7 +11,22 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    //Create scanner for user input 
+    Scanner input = new Scanner(System.in); 
+
+    //prompt the user to enter in a number between 1-10 and declare variable for user's number 
+    System.out.println("Please enter a number between 1 and 10"); 
+    int userNum = input.nextInt(); 
+
+    //create nest loop for program 
+    for(int count = 1; count <= userNum; count++){
+      for(int count2 = 1; count2 <= userNum; count2++){
+         System.out.print("*"); 
+      }
+      System.out.println();
+    } 
+    
+
     
   }
 }
